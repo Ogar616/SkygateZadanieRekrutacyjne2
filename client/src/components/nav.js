e72a73c1a4be2a5ca805ad566c8dbbb2d321fea8
Ogar <kamil.sobczyk@wp.pl>
2019-03-14 12:53:49 +0100

    simple nav draft prepared

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,17 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import PeopleIcon from "@material-ui/icons/People";
-import FaceIcon from "@material-ui/icons/Face";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from '@material-ui/icons/Search';
 
-const styles = {
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
   root: {
-    flexGrow: 1
+    width: "100%"
   },
   grow: {
     flexGrow: 1
@@ -19,8 +19,65 @@ const styles = {
   menuButton: {
     marginLeft: -12,
     marginRight: 20
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%"
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
+    }
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
   }
-};
+});
 
 const Navbar = props => {
   const { classes } = props;
@@ -28,23 +85,21 @@ const Navbar = props => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="People"
-          >
-            <PeopleIcon />
-          </IconButton>
           <Typography variant="h6" color="inherit" className={classes.grow}>
-            Shopping List
+            Skygate - recruitment task
           </Typography>
-          <IconButton
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="People"
-          >
-            <FaceIcon />
-          </IconButton>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+            />{" "}
+          </div>
         </Toolbar>
       </AppBar>
     </div>
