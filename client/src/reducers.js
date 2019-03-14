e5b16697cadb5c1aa097e6ffab96379c5a9914e8
Ogar <kamil.sobczyk@wp.pl>
2019-03-14 13:43:45 +0100

    get description action added

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -1,12 +1,15 @@
 const initialState = {
-cities: []
+cities: [],
+descriptions: []
 };
 
 const reducer = (state = initialState, action) => {
-  const { cities } = action;
+  const { cities, description } = action;
   switch (action.type) {
     case "GET_CITIES":
       return { ...state, cities };
+    case "GET_DESCRIPTION":
+      return { ...state, description };
     default:
       return state;
   }
