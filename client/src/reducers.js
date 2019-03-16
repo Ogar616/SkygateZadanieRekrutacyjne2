bb8f19ba3d648631c1b600c3783e3403d71e70ce
Ogar <kamil.sobczyk@wp.pl>
2019-03-16 17:02:06 +0100

    getDescription method in redux rebuilded

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -5,13 +5,13 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  const { cities, inputValue, locations } = action;
+  const { cities, inputValue, locations, descriptions } = action;
   switch (action.type) {
     case "GET_CITIES":
       return { ...state, cities };
     case "GET_DESCRIPTIONS":
-      console.log(locations);
-      return { ...state };
+      console.log("loc", descriptions);
+      return { ...state, descriptions };
     case "GET_INPUT_VALUE":
       console.log("reducer inputV", state.inputValue);
       return { ...state, inputValue };
