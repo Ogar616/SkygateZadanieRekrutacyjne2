f0477008cf98d4a3f0c0ea93368e29f84f17efc9
Ogar <kamil.sobczyk@wp.pl>
2019-03-16 21:37:08 +0100

    change input method added, switch with country codes addded

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -1,22 +1,37 @@
 const initialState = {
   cities: [],
-  descriptions: [],
   inputValue: ""
 };
 
 const reducer = (state = initialState, action) => {
-  const { cities, inputValue, city } = action;
+  const { cities, value, city } = action;
   switch (action.type) {
     case "GET_CITIES":
       return { ...state, cities };
     case "ADD_CITY":
-    console.log("!!!!", city);
       const newCities = [...state.cities];
       newCities.push(city);
       return { ...state, cities: newCities };
-    case "GET_INPUT_VALUE":
-      console.log("reducer inputV", state.inputValue);
-      return { ...state, inputValue };
+    case "CHANGE_INPUT_VALUE":
+      console.log("reducer inputV", value);
+      let code;
+      switch (value.label) {
+        case "Germany":
+          code = "GER";
+          break;
+        case "Poland":
+          code = "PL";
+          break;
+        case "France":
+          code = "FR";
+          break;
+        case "SPAIN":
+          code = "ES";
+          break;
+        default:
+          code = "PL";
+      }
+      return { ...state, inputValue: code };
     case "SET_INPUT_VALUE":
       const newValue = action.inputValue;
       console.log("newI reducer", newValue);
