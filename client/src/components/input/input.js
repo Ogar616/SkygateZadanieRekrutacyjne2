ee0caebdaceef7bdb620aef65316ed746e9b5c52
Ogar <kamil.sobczyk@wp.pl>
2019-03-16 21:38:03 +0100

    input components moved to separate file

---
+++ client/src/components/input/input.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Select from "react-select";
+
+import { withStyles } from "@material-ui/core/styles";
+import { emphasize } from "@material-ui/core/styles/colorManipulator";
+
+import { components, suggestions } from "./inputComponents";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 250,
+    width: 500,
+    margin: "0 auto"
+  },
+  input: {
+    display: "flex",
+    padding: 0
+  },
+  valueContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    flex: 1,
+    alignItems: "center",
+    overflow: "hidden"
+  },
+  chip: {
+    margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`
+  },
+  chipFocused: {
+    backgroundColor: emphasize(
+      theme.palette.type === "light"
+        ? theme.palette.grey[300]
+        : theme.palette.grey[700],
+      0.08
+    )
+  },
+  noOptionsMessage: {
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`
+  },
+  singleValue: {
+    fontSize: 16
+  },
+  placeholder: {
+    position: "absolute",
+    left: 2,
+    fontSize: 16
+  },
+  paper: {
+    position: "absolute",
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0
+  },
+  divider: {
+    height: theme.spacing.unit * 2
+  }
+});
+
+class Input extends Component {
+  state = {
+    country: null,
+    multi: null
+  };
+
+  render() {
+    const { classes, theme, changeInputValue } = this.props;
+
+    const selectStyles = {
+      input: base => ({
+        ...base,
+        color: theme.palette.text.primary,
+        "& input": {
+          font: "inherit"
+        }
+      })
+    };
+
+    return (
+      <div className={classes.root}>
+        <Select
+          classes={classes}
+          styles={selectStyles}
+          options={suggestions}
+          components={components}
+          onChange={changeInputValue}
+          placeholder="Search a country"
+          isClearable
+        />
+        <div className={classes.divider} />
+      </div>
+    );
+  }
+}
+
+Input.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  return { inputValue: state.inputValue };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeInputValue: value => dispatch({ type: "CHANGE_INPUT_VALUE", value })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles, { withTheme: true })(Input));
