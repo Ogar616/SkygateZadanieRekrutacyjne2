8562df24230f03199d409c4f462926e1509155da
Ogar <kamil.sobczyk@wp.pl>
2019-03-16 19:55:02 +0100

    input functions removed

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const openaq =
   "https://api.openaq.org/v1/locations?parameters=pm10&order_by=count&sort=desc&limit=10&country=";
 const wiki =
-  "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&explaintext&format=json&redirects&titles=";
-
-const server = "http://localhost:3001/";
+  "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&explaintext&format=json&category=city&redirects&titles=";
 
 const fetchData = () => {
   return new Promise((resolve, reject) => {
@@ -26,46 +24,14 @@ const getData = (country, addCity) => {
               id = key;
             }
             const description = response.data.query.pages[id].extract;
-            addCity({city, description});
+            addCity({ city, description });
           })
           .catch(error => {
             console.log("Ooops ", error);
           });
       });
-    })
+    });
   });
 };
 
-const getInputValue = dispatch => {
-  fetch(server + "input", {
-    mode: "cors",
-    method: "GET"
-  })
-    .then(response => {
-      return response.json();
-    })
-    .then(inputValue => {
-      return dispatch(inputValue);
-    });
-};
-
-const putInputValue = newInput => {
-  fetch(server + "input", {
-    mode: "cors",
-    method: "PUT",
-    headers: {
-      "Content-type": "application/json"
-    },
-    body: newInput
-  })
-    .then(response => {
-      return response.json();
-    })
-    .then(inputValue => {
-      console.log("newInputValue", inputValue);
-      return inputValue;
-    })
-    .catch(error => console.log("Ooops", error));
-};
-
-export { getData, getInputValue, putInputValue };
+export default getData;
