cd7395d22ba6c1f17dfab73eced514a3d4100472
Ogar <kamil.sobczyk@wp.pl>
2019-03-16 16:32:32 +0100

    getDescrptions reduser changed

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -1,15 +1,24 @@
 const initialState = {
-cities: [],
-descriptions: []
+  cities: [],
+  descriptions: [],
+  inputValue: ""
 };
 
 const reducer = (state = initialState, action) => {
-  const { cities, description } = action;
+  const { cities, inputValue, locations } = action;
   switch (action.type) {
     case "GET_CITIES":
       return { ...state, cities };
-    case "GET_DESCRIPTION":
-      return { ...state, description };
+    case "GET_DESCRIPTIONS":
+      console.log(locations);
+      return { ...state };
+    case "GET_INPUT_VALUE":
+      console.log("reducer inputV", state.inputValue);
+      return { ...state, inputValue };
+    case "SET_INPUT_VALUE":
+      const newValue = action.inputValue;
+      console.log("newI reducer", newValue);
+      return { ...state, inputValue: newValue };
     default:
       return state;
   }
