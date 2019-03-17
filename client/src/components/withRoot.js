4ed4f6bc30c802e26fda57763a17f16c7b503a82
Ogar <kamil.sobczyk@wp.pl>
2019-03-17 12:57:04 +0100

    theme changed

--- client/src/components/withRoot.js
+++ client/src/components/withRoot.js
@@ -1,21 +1,15 @@
 import React from "react";
 
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import purple from "@material-ui/core/colors/purple";
-import green from "@material-ui/core/colors/green";
+import blue from "@material-ui/core/colors/blue";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700]
-    },
-    secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700]
+      light: blue[300],
+      main: blue[500],
+      dark: blue[700]
     }
   },
   typography: {
