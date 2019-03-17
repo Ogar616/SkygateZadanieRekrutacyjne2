42fcadd95b1ac1be7d5ae0a3e9e86ea92b693385
Ogar <kamil.sobczyk@wp.pl>
2019-03-17 12:32:53 +0100

    comment upgraded

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -1,5 +1,8 @@
 //openaq API is not working properly when used proper way according to documentation. "parameters=bc" query is giving different pollutions measurments in every record. Odrering measurments by for example by pm10 is not working as well. https://api.openaq.org/v1/measurements?parameters=pm10 - with any parameter is not giving any records - gateway tiemout. It is not a only one case. During app development few times API did not responded for about 15-20 minutes at all. To get app working I used LIST OF STATIONS (name-of-the-city - name-of-measurement-station)in chosen country sorted by count of measurements - descending witch is sometimes working :).
 //Wikipedia API is highly unstructured. In some cases cities descriptions are descriptions of other things with the same names - especially in countries different than Poland and Germany.
+//I've had 5 day for this task, and lost more than 2 for struggling with those APIs - I think you just gave it on purpose to see how I will handle this. I didn't want to use mockups, cause I wanted to use real endpoint.
+//I asked Agnieszka Dombrowska for about 2 weekes for this task, and it was not ready. Then I received it in a suprising moment when I was terribly busy, so I did it as it is in 4 days. Because of that reasons UX is not perfect, it is basically a draft - I am sure that you know how easy you can build nice looking user interfaces in Material UI.
+//If it's possible please give me some informations/code review before friday march 22th - I have some other work possibilities and I need to make some decisions. I make no secret of the fact that Skygate is the most desired company for me to start with :)
 
 import axios from "axios";
 
